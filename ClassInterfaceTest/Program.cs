db3fb649c72440017200f58e9c8b7ccbd4740c58
Henrik R. Mortensen <h2m@outlook.com>
2015-02-22 16:06:58 -0800

    First

---
+++ ClassInterfaceTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInterfaceTest
+{
+    public class Note
+    {
+        int _NoteIx = 0;
+
+
+        Note(int IX)
+        {
+            _NoteIx = IX;
+        }
+
+
+        public class Line
+        {
+            int _NoteLineIx = 0;
+
+            Line(int IX)
+            {
+                _NoteLineIx = IX;
+            }
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
